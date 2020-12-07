4babc21b94bc9790be55febffe9599bad55fd702
berhane beyene <monoxica2004@yahoo.com>
2020-12-06 23:23:38 -0600

    some text on the home page

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+//all our activities is going to conneted to this page 
 class Home extends Component {
     // constructor() {
     //     super()
@@ -12,6 +12,8 @@ class Home extends Component {
         }
         return (
             <div>
+                <h1>Welcome to the home page</h1>
+                <h1>all our activities is going to conneted to this page </h1>
                 <p>It's good to be home</p>
                 <img style={imageStyle} src="https://i.ytimg.com/vi/N1icEHtgb3g/maxresdefault.jpg" alt="myImage" />
             </div>
